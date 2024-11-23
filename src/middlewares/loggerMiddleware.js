b4a34bb62c5c19f278079e8d90e9ea502ea2d534
Daniel Ambrosim Colodete <63670844+danielambrosim@users.noreply.github.com>
2024-11-22 23:11:13 -0300

    Delete src directory

--- src/middlewares/loggerMiddleware.js
+++
@@ -1,7 +0,0 @@
-// middlewares/loggerMiddleware.js
-const loggerMiddleware = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-};
-
-module.exports = loggerMiddleware;
